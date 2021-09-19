ac958664085d41eb596ee835c6059600ccc9342d
Babji Rao <babji.rao.kajji@macys.com>
2021-09-19 12:39:36 -0400

    Added Project

--- NetworkCallSample/ViewController.swift
+++ NetworkCallSample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "NetworkCall"
         // Do any additional setup after loading the view.
     }
 
