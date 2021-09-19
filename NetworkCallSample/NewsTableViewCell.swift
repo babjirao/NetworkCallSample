6b0b19ec63f6ec05a20e2a8057685c139f499d8e
Babji Rao <babji.rao.kajji@macys.com>
2021-09-19 12:58:17 -0400

    Added network tableview

---
+++ NetworkCallSample/NewsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewsTableViewCell.swift
+//  NetworkCall
+//
+//  Created by Babji Rao Kajji (contractor) on 8/29/21.
+//  Copyright © 2021 Babji Rao Kajji (contractor). All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- NetworkCallSample/ViewController.swift
+++ NetworkCallSample/ViewController.swift
@@ -1,21 +1,54 @@
-//
-//  ViewController.swift
-//  NetworkCallSample
-//
-//  Created by Babji Rao Kajji (contractor) on 7/1/21.
-//  Copyright © 2021 Babji Rao Kajji (contractor). All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+   
+    @IBOutlet weak var newsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "NetworkCall"
+        self.title = "News"
         // Do any additional setup after loading the view.
+        self.newsTableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTableViewCell")
+        self.callApi()
+
     }
 
+    func callApi() {
+
+        var request = URLRequest(url: URL(string: "https://www.reddit.com/.json")!)
+        request.httpMethod = "GET"
+       // request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            print(response!)
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                print(json["data"])
+            } catch {
+                print("error")
+            }
+        })
+
+        task.resume()
+    }
+
+
+}
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           4
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath) as? NewsTableViewCell else {fatalError("unable to load the cell")}
+        return cell
+       }
+       
 
 }
 
